752dc4b5e76fca4c9a0f07f83b7ee0cea7eaf433
Blake Howes <blakehowes1@gmail.com>
2020-11-06 15:30:48 +1300

    RocketTurret start

---
+++ Assets/Rocket.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float boost;
+    public float thrust;
+    void OnEnable()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.TransformDirection(-Vector3.up)*boost);
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/RocketTurret.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketTurret : MonoBehaviour
+{
+    public float range;
+    public LayerMask playermask;
+    float checktplayertimer;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        checktplayertimer += Time.deltaTime;
+        if(checktplayertimer > 1)
+        {
+            playercheck();
+            checktplayertimer = 0f;
+        }
+    }
+
+    private bool playercheck()
+    {
+        bool isplayerinrange = false;
+        if(Physics2D.OverlapCircle(transform.position, range, playermask))
+        {
+            isplayerinrange = true;
+            Debug.Log("Inrange");
+        }
+        return isplayerinrange;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
