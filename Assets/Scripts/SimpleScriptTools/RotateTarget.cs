79d99fdaf1fb1617ecc21c2e78ecf2890af28fdf
Blake Howes <blakehowes1@gmail.com>
2020-11-12 18:52:27 +1300

    Its now a car game!

---
+++ Assets/Scripts/SimpleScriptTools/RotateTarget.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTarget : MonoBehaviour
+{
+    public GameObject target;
+    void FixedUpdate()
+    {
+        transform.right = target.transform.position - transform.position;
+    }
+}
