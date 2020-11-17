0a3f37e238f7c2aa0db634abe64e9155624bd4db
peeyt <peetvipz@gmail.com>
2020-11-17 14:27:06 +1300

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/SimpleScriptTools/RotateTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTest : MonoBehaviour
+{
+    public float rot1;
+    public float rot2;
+    public float rot3;
+    public float speed;
+    public float speed2;
+    void FixedUpdate()
+    {
+        rot1 += Time.deltaTime * speed;
+        rot2 += Time.deltaTime * speed2;
+        Quaternion rot = Quaternion.Euler(rot1, rot2, rot3);
+        transform.rotation = rot;
+    }
+}
