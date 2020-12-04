42bd88018f5d6e8755781ff60fd9aaeafe79b05b
Blake Howes <blakehowes1@gmail.com>
2020-12-04 15:29:46 +1300

    Changes

--- Assets/Scripts/Tools/Spawn.cs
+++ Assets/Scripts/Tools/Spawn.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
-    private float spawmtimer;
+    public float spawmtimer;
     public float SpawnRate;
     public float RandomRange;
     public float ScaleMax;
     public float ScaleMin;
+    public LayerMask triggerzone;
     public List<GameObject> Items = new List<GameObject>();
     void Update()
     {
+
         spawmtimer += Time.deltaTime;
-        if(spawmtimer > Random.Range(SpawnRate - RandomRange, SpawnRate + RandomRange))
+        if (spawmtimer > Random.Range(SpawnRate - RandomRange, SpawnRate + RandomRange))
         {
-            var spawnedobject = Instantiate(Items[Random.Range(0,Items.Count)], transform.position, Quaternion.identity);
-            spawnedobject.transform.localScale = spawnedobject.transform.localScale * Random.Range(ScaleMax, ScaleMin);
-            spawmtimer = 0f;
+            Collider2D hitColliders = Physics2D.OverlapBox(gameObject.transform.position, transform.localScale, 0f, triggerzone);
+            if (hitColliders != null)
+            {
+                var spawnedobject = Instantiate(Items[Random.Range(0, Items.Count)], transform.position, Quaternion.identity);
+                spawnedobject.transform.localScale = spawnedobject.transform.localScale * Random.Range(ScaleMax, ScaleMin);
+                spawmtimer = 0f;
+            }
+            else
+            {
+                spawmtimer = 0;
+            }
+
         }
+
     }
 }
+
+
