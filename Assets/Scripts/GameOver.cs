8acb87a42b3e7e9b8291aa4c023ce93e8266fcde
Jet Yong <jettlag2000@gmail.com>
2020-12-08 14:28:55 +1300

    Gameover

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject gameOver;
+    public GameObject carRoot;
+
+    void Update()
+    {
+        if (carRoot == null)
+        {
+            StartCoroutine(PlayerDeath());
+        }
+    }
+
+    IEnumerator PlayerDeath()
+    {
+        yield return new WaitForSeconds(2);
+        gameOver.SetActive(true);
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("LevelBuildJet");
+    }
+
+}
